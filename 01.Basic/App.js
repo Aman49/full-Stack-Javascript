7b8c93b0671900e1640e47e2a752858ba3bd0897
Aman WebDev <amanshrivast49@gmail.com>
2024-02-11 15:27:46 +0530

    console & comments

---
+++ 01.Basic/App.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+console.warn("This is an warning");
+console.error("This is an error text");
+console.table({ name: "Aman", Age: 23 });
+
+// This is an single line Comment
+/* This is an multiline Comment 
+and It can be used to comment in multi
+lines */
