a8d23480f01aefff06335fd9ca54b6a4090ce111
Aman WebDev <amanshrivast49@gmail.com>
2024-02-11 16:20:05 +0530

    comparisonOperator

---
+++ 02.Primittive/comparisonOperator/app.js
@@ -0,0 +1,18 @@
+/* Comparison operators are of two types 
+relational operator >, >=, <, <=
+Equaility operator
+=== strict equility (value + type)
+!== strict inequility (value + type)
+== loose equility (value)
+!= loose inequility (value)
+*/
+let firstFavNum = 21;
+let secondFavNum = 10;
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum != secondFavNum);
