39bae4e9a64369abf1f570cf8f08e1667401f431
Aman WebDev <amanshrivast49@gmail.com>
2024-02-11 15:37:55 +0530

    variables

---
+++ 02.Primittive/variable/variables.js
@@ -0,0 +1,17 @@
+/* 
+Rules for variables names
+ it cannot start with numbers
+ it does not include space between
+ it should not use - variable name
+ variable name can start with _ and $
+ It should not be same as js keyword
+ */
+
+let name = "Aman kumar";
+const whatDoYouWannaBecomeInLife = "Programmer";
+let gender = "Male";
+const github = "https://github.com/Aman49";
+console.log(name);
+console.log(gender);
+console.log(gender);
+console.log(github);
