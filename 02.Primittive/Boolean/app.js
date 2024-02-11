61377ee73e43324546d70bf5e1d7e4de2b4ab784
Aman WebDev <amanshrivast49@gmail.com>
2024-02-11 15:59:15 +0530

    booleans

---
+++ 02.Primittive/Boolean/app.js
@@ -0,0 +1,16 @@
+/* 
+falsy values
+fasle, 
+null, 
+undefined,
+0,
+-0,
+NaN,
+'', "", ``, (empty quotes)
+*/
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNum = 10;
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNum + undefined);
