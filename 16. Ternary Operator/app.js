d3071d9e2ff3a53c62d4922b929eee38d8b0a4bd
Aman WebDev <amanshrivast49@gmail.com>
2024-02-13 19:32:07 +0530

    second Time

--- 16. Ternary Operator/app.js
+++ 16. Ternary Operator/app.js
@@ -46,8 +46,11 @@ console.log(passwordChecker(password));
 // ternary operator to check the eligibility to vote
 
 let age = 15;
-let result;
-age >= 18
-  ? (result = "You are eligible to vote.")
-  : (result = "You are not eligible to vote yet.");
+let result =
+  age >= 18 ? "You are eligible to vote." : "You are not eligible to vote yet";
 console.log(result);
+
+// Create a program which checks if number is positive, negative or zero
+let a = 3;
+let res = a >= 0 ? (a == 0 ? "zero" : "positive") : "negative";
+console.log(`The number is ${res}.`);
