b54e1082e4875443d8a7dc48bfb06bcaee74081b
Aman WebDev <amanshrivast49@gmail.com>
2024-02-16 23:47:38 +0530

    innerHtml, innerText, textContent

---
+++ 28. DOM/02. innerText, innerHTML, textContent/app.js
@@ -0,0 +1,12 @@
+// 1. Check the innerText of "first" element
+// 2. Check the textContent of "second" element
+// 3. Check the innerHTML of "third" element
+
+let first = document.querySelector(".first");
+console.log(first.innerText);
+
+let sec = document.querySelector(".second");
+console.log(sec.textContent);
+
+let third = document.querySelector(".third");
+console.log(third.innerHTML);
