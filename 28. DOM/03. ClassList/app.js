26610e5a03baea7d722093a3747895b33533eb65
Aman WebDev <amanshrivast49@gmail.com>
2024-02-17 08:48:18 +0530

    class List

---
+++ 28. DOM/03. ClassList/app.js
@@ -0,0 +1,12 @@
+// classlist
+// add()
+// remove()
+// toggle()
+
+let h1 = document.querySelector("h1");
+console.log(h1.classList);
+
+h1.classList.add("bg");
+h1.classList.remove("bg");
+
+h1.classList.toggle("bg");
