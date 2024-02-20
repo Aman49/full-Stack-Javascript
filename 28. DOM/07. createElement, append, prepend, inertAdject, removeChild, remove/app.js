a7944d5741fcc34f97c87bd9edd7a2f20e0d1fcb
Aman WebDev <amanshrivast49@gmail.com>
2024-02-20 10:19:59 +0530

    correction

--- 28. DOM/07. createElement, append, prepend, inertAdject, removeChild, remove/app.js
+++ 28. DOM/07. createElement, append, prepend, inertAdject, removeChild, remove/app.js
@@ -16,9 +16,54 @@
 // removeChild()
 // remove()
 // ------------------------------------
-let h1 = document.createElement("h1");
-h1.style.color = "pink";
-h1.innerText = "I am created Element";
-h1.style.display = "inline";
-let ul = document.querySelector(".new-list");
-ul.beforeBegin(h1);
+
+// ------------------------------------
+// Creating Element
+// const h1 = document.createElement("h1");
+// const body = document.body;
+
+// // Adding Text
+// h1.textContent = "Hello World";
+
+// // Attaching element to the body
+// body.appendChild(h1);
+// ------------------------------------
+
+// ------------------------------------
+const ul = document.querySelector("ul");
+
+// ------------ appendChild() ------------
+const newLi = document.createElement("li");
+newLi.innerText = "I'm li tag";
+ul.appendChild(newLi);
+
+// ------------ insertBefore(what, where) ------------
+const firstLi = document.querySelector("li");
+ul.insertBefore(newLi, firstLi);
+
+// ------------ insertAdjacentElement(where, what) ------------
+const firstP = document.querySelector("p");
+const i = document.createElement("i");
+i.innerText = "I'm italics";
+i.style.color = "skyblue";
+firstP.insertAdjacentElement("beforebegin", i);
+firstP.insertAdjacentElement("afterbegin", i);
+firstP.insertAdjacentElement("beforeend", i);
+firstP.insertAdjacentElement("afterend", i);
+
+// ------------------ Append() ------------------
+// In append method you can pass more then one element
+let section = document.querySelector("section");
+section.append(i, firstLi);
+
+// ----------- Prepend() -----------
+// You can still pass multiple elements if you want to, but it will push it at the top.
+let newList = document.querySelector(".new-list");
+let a = document.createElement("a");
+a.textContent = "HuXn WebDev";
+a.href = "www.youtube.com/@huxnwebdev";
+newList.prepend(a);
+
+// ----------- removeChild() -----------
+newList.removeChild(a);
+newList.remove();
